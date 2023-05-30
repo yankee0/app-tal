7fc4a253c7c97645f7fad735979121e0935b4127
yankee <yankeesuprem@gmail.com>
2023-05-30 20:01:08 +0000

    migration chauffeurs livraisons

---
+++ app/Database/Migrations/2023-05-30-192844_Chauffeurs.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Chauffeurs extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'matricule' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'null' => false,
+            ],
+            'prenom' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'null' => true,
+            ],
+            'nom' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'null' => true,
+            ],
+            'tel' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'null' => true,
+            ],
+        ]);
+        $this->forge->addPrimaryKey('id');
+        $this->forge->addUniqueKey('matricule');
+        $this->forge->createTable('chauffeurs');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('chauffeurs');
+    }
+}
