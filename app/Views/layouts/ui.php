52bd3a4a1bec3eed265e007d9d671065a19792e8
yankee <yankeesuprem@gmail.com>
2023-07-25 09:18:10 +0000

    back to normal

--- app/Views/layouts/ui.php
+++ app/Views/layouts/ui.php
@@ -53,11 +53,13 @@
   <?php endif ?>
   <div class="min-height-300 bg-primary position-absolute w-100"></div>
   <aside class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 " id="sidenav-main">
-    
-  <div class="d-flex justify-content-center">
-
-    <img src="<?= base_url('assets/img/logo.jpg') ?>"  id="logoPic" alt="Logo">
-  </div>
+    <div class="sidenav-header">
+      <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
+      <a class="navbar-brand m-0" href="<?= base_url(session()->root) ?>">
+        <img src="<?= base_url('assets/img/logo.jpg') ?>" class="navbar-brand-img h-100" alt="Logo">
+        <span class="ms-1 font-weight-bold">TAL APP</span>
+      </a>
+    </div>
     <hr class="horizontal dark mt-0">
     <!-- <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main"> -->
     <ul class="navbar-nav">
